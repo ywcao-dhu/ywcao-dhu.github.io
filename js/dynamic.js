bc88e6e84b1ba8f7b92a4859eaf170c110b2370b
ywcao-lab <98568832+ywcao-lab@users.noreply.github.com>
2023-10-12 09:20:01 +0800

    Update dynamic.js

--- js/dynamic.js
+++ js/dynamic.js
@@ -2,7 +2,8 @@
 let rearchpros=[
     `Congratulations! Our manuscript has been accepted for publication in the IEEE TCOM. [J1] Y. Cao, S. Maghsudi, T. Ohtsuki and T. Q. S. Quek, "Mobility-Aware Routing and Caching in Small Cell Networks using Federated Learning," in IEEE Transactions on Communications.`,
     "mmwave",
-    "transfer learning"
+    "transfer learning",
+	"machine learning"
 ]
 let rearch=document.getElementById("re1");
 let publishlist=document.getElementById('publishes')
@@ -35,7 +36,7 @@ rearchpros.forEach((val,index,array)=>{
       <div class="cc-porfolio-image img-raised aos-init aos-animate" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
             <div style="background-image: url('images/project-1.jpg');width: 100%;height: 40vh;background-size: cover;">
               <div class="h4 title text-white text-center">Recent Project</div>
-              <p class="title text-white">&nbsp;&nbsp;&nbsp;&nbsp;${array[index+1]}</p>
+              <p class="title text-black">&nbsp;&nbsp;&nbsp;&nbsp;${array[index+1]}</p>
             </div>
     </div>
     </div>
